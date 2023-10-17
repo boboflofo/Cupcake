f24286e7edf6a516df2fbb2df3c80581320ec33f
Jonathan Song <jonats10@uci.edu>
2023-10-17 15:25:56 -0700

    add script window onload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,44 @@
-function light() {
-  document.body.style.backgroundColor = "white";
-  document.body.style.color = "black";
+window.onload = function() {
+let light = document.querySelector(".light-mode");
+    light.onclick = function() {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+    }
+
+
+let dark = document.querySelector(".dark-mode");
+dark.onclick = function() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
 }
 
-function dark() {
-  document.body.style.backgroundColor = "black";
-  document.body.style.color = "white";
+let increase = document.querySelector(".increase");
+    increase.onclick = function() {
+    document.body.style.fontSize = "50px";
+    }
+
+
+let decrease = document.querySelector(".decrease");
+    decrease.onclick = function() {
+    document.body.style.fontSize = "15px";
+}
 }
 
-function increaseSize() {
-  document.body.style.fontSize = "50px"
-}
+    let h1MouseOn = document.querySelector("h1");
+        h1.mouseover = function() {
+        document.querySelector("h1").backgroundColor = "yellow";
+    }
 
-function decreaseSize() {
-  document.body.style.fontSize = "10px"
-}
+    // let h1MouseOut = document.querySelector("h1");
+    //     h1.onmouseout = function() {
+    //     h1.style. backgroundColor = "white";
+    //     }
 
+    let h1 = document.querySelector("h1");
+        h1.onmouseover = function() {
+        window.alert("I am a heading element.");
+};
 
 // document.querySelector(".light-mode").onclick = light()
+
+button.className = "light-mode" 
