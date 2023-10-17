9f349d169603d37116cc6a3f6af2b7ba0aac2d7b
Jonathan Song <jonats10@uci.edu>
2023-10-17 14:26:15 -0700

    add functions for light and dark mode

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function light() {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+}
+
+function dark() {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+}
+
+function increaseSize() {
+  document.body.style.fontSize = "50px"
+}
+
+function decreaseSize() {
+  document.body.style.fontSize = "10px"
+}
+
+
+// document.querySelector(".light-mode").onclick = light()
